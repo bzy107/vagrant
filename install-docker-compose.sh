16aa495fcf5c6fe232d5ef982c0969ea8141e99e
bzy107 <bzy107@gmail.com>
2023-11-04 20:19:26 +0900

    dockerのアンインストールのコマンドの追加 不要な記載削除

--- install-docker-compose.sh
+++ install-docker-compose.sh
@@ -1,57 +1,14 @@
 #!/bin/bash
 
-# bargeの設定
-# sudo pkg install vim
-# # sudo pkg install git
-# sudo pkg install git -e BR2_PACKAGE_OPENSSL=y -e BR2_PACKAGE_LIBCURL=y
-# git config --global http.sslCAinfo /etc/ssl/certs/ca-certificates.crt
-
-# sudo pkg install make
-
-# # curlインストール bargee最新にしたらできなかった
-# # sudo pkg install libcurl -e BR2_PACKAGE_OPENSSL=y -e BR2_PACKAGE_CURL=y
-# # export CURL_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
-
-
-# # bargeのdockerを最新化
-# sudo /etc/init.d/docker restart latest
-
-# sudo wget "https://github.com/docker/compose/releases/download/v2.6.0/docker-compose-$(uname -s)-$(uname -m)" -O /usr/bin/docker-compose
-# sudo chmod +x /usr/bin/docker-compose
-
 # ubuntuの設定
 sudo apt update
 
 # makeのインストール
 sudo apt install make
 
-# dockerのインストール
-# # sudo apt install docker.io
-# sudo snap install docker
+# 競合パッケージアンインストール
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
 
-# # Docker Compose install
-# # 以下ドキュメントからコマンド抜粋
-# # https://docs.docker.com/compose/install/compose-plugin/
-
-# # DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
-# DOCKER_CONFIG='/home/vagrant/.docker'
-# DOCKER_VERSION='v2.23.0'
-# # https://github.com/docker/compose/releases
-# # 最新versionはここから確認
-
-# sudo mkdir -p $DOCKER_CONFIG/cli-plugins
-# sudo wget https://github.com/docker/compose/releases/download/$DOCKER_VERSION/docker-compose-$(uname -s)-$(uname -m) -O $DOCKER_CONFIG/cli-plugins/docker-compose
-# sudo chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
-
-
-
-# dockerのインストール 20231102以降
-# 20231102 
-# docker compose -p armureblanche exec php /bin/bash でコンテナに入れない以下エラーが発生
-# http: invalid Host header
-
-# そのため以下のマニュアルに沿ってdockerのインストール
-# https://docs.docker.com/engine/install/ubuntu/
 
 sudo apt-get update
 sudo apt-get install ca-certificates curl gnupg
